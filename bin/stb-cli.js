586327ab711c9319f28220ed448e96ba45a09b5b
DarkPark <darkpark@pisem.net>
2014-12-13 13:54:35 +0200

    rework gulp tasks loading mechanism

--- bin/stb-cli.js
+++ bin/stb-cli.js
@@ -9,22 +9,41 @@
 
 'use strict';
 
-var path      = require('path'),
+var fs        = require('fs'),
+	path      = require('path'),
 	gulp      = require('gulp'),
 	gutil     = require('gulp-util'),
 	cliParams = require('minimist')(process.argv.slice(2)),
 	task;
 
 
+/**
+ * Load all js modules from the given directory.
+ *
+ * @param {string} dir folder with modules
+ */
+function load ( dir ) {
+	// may be missing
+	if ( fs.existsSync(dir) ) {
+		// get file list and walk through it
+		require('fs').readdirSync(dir).forEach(function ( name ) {
+			// make correct absolute path
+			name = path.join(dir, name);
+			// check file type
+			if ( path.extname(name) === '.js' ) {
+				require(name);
+			}
+		});
+	}
+}
+
 // set env vars for current working dir
 // and the dir with stb-cli package
 process.env.CWD = process.cwd();
 process.env.STB = path.normalize(__dirname + '/..');
 
-// iterates over the specified directory requiring each file
-require('fs').readdirSync(path.join(process.env.STB, 'tasks')).forEach(function ( name ) {
-	require(path.join(process.env.STB, 'tasks', name));
-});
+// try to load base gulp tasks
+load(path.join(process.env.STB, 'tasks'));
 
 // task specified by user to execute
 task = gulp.tasks[cliParams['_'][0]];
