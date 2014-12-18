61abdfbe16bffa07b669b06044531ec060cd2ed0
Stanislav Kalashnik <sk@infomir.eu>
2014-12-18 14:40:20 +0200

    optimize init process

--- tasks/init.js
+++ tasks/init.js
@@ -9,66 +9,26 @@
 
 'use strict';
 
-var fs    = require('fs'),
-	npm   = require('npm'),
-	path  = require('path'),
-	gulp  = require('gulp');
+var gulp     = require('gulp'),
+	conflict = require('gulp-conflict'),
+	files    = [
+		process.env.STB + '/tpl/**',
+		process.env.STB + '/license.md',
+		process.env.STB + '/.eslintrc',
+		process.env.STB + '/.editorconfig'
+	];
 
 
 gulp.task('init', function ( done ) {
 	// copy template files to the current dir
-	gulp.src([process.env.STB + '/tpl/**', process.env.STB + '/.eslintrc', process.env.STB + '/.editorconfig'])
-		.pipe(require('gulp-conflict')('./'))
+	gulp.src(files, {dot: true})
+		.pipe(conflict('./'))
 		.pipe(gulp.dest('./'))
 		.on('end', function () {
 			// copy config files to the current dir
 			gulp.src(process.env.STB + '/config/**', {base: process.env.STB})
-				.pipe(require('gulp-conflict')('./'))
+				.pipe(conflict('./'))
 				.pipe(gulp.dest('./'))
-				.on('end', function () {
-					// manual empty dirs creation
-					fs.mkdirSync('./build/develop');
-					fs.mkdirSync('./build/release');
-					fs.mkdirSync('./build/develop/css');
-					fs.mkdirSync('./build/release/css');
-
-					done();
-
-					npm.load({loaded: false}, function ( error ) {
-						var config = require(path.join(process.env.CWD, 'package.json')),
-							pkgSet = [];
-
-						if ( error ) {
-							console.log(error);
-							return;
-						}
-
-						// section is present
-						if ( typeof config.dependencies === 'object' ) {
-							// build package name@version list
-							Object.keys(config.dependencies).forEach(function ( name ) {
-								pkgSet.push(name + '@' + config.dependencies[name]);
-							});
-
-							// there are some packages to install
-							if ( pkgSet.length > 0 ) {
-								console.log('\ninstalling dependencies ...'.green);
-
-								// do the installation
-								npm.commands.install(pkgSet, function ( error ) {
-									if ( error ) {
-										console.log(error);
-									} else {
-										console.log('done'.green);
-									}
-								});
-								npm.on('log', function ( message ) {
-									// log the progress of the installation
-									console.log(message);
-								});
-							}
-						}
-					});
-				});
+				.on('end', done);
 		});
 });
