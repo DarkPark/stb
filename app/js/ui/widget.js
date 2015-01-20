7696517f53ad41d63f5253c53469a93bc66ec0d5
Stanislav Kalashnik <sk@infomir.eu>
2015-01-20 14:37:28 +0200

    implement widget component

--- app/js/ui/widget.js
+++ app/js/ui/widget.js
@@ -1,6 +1,4 @@
 /**
- *
- *
  * @module stb/ui/widget
  * @author Stanislav Kalashnik <sk@infomir.eu>
  * @license GNU GENERAL PUBLIC LICENSE Version 3
@@ -12,62 +10,41 @@ var Component = require('../component');
 
 
 /**
- * Base page implementation.
+ * Base widget implementation.
  *
- * A full-screen top-level layer that can operate as an independent separate entity.
- * It is added to the document body on creation if not already linked.
+ * A part-screen top-level layer that can operate as an independent separate entity.
  *
  * @constructor
  * @extends Component
  *
  * @param {Object} [config={}] init parameters (all inherited from the parent)
+ * @param {boolean} [config.visible=false] component initial visibility state flag
+ * @param {boolean} [config.focusable=false] component can accept focus or not
  *
  * @example
- * var Page = require('stb/ui/page'),
- *     page = new Page({
+ * var Widget = require('stb/ui/widget'),
+ *     widget = new Widget({
  *         $node: document.getElementById(id)
  *     });
  *
- * page.addListener('show', function show () {
- *     // page is visible now
- * });
+ * // somewhere
+ * widget.show();
  */
 function Widget ( config ) {
-	/**
-	 * Page visibility/active state flag.
-	 *
-	 * @readonly
-	 * @type {boolean}
-	 */
-	//this.active = false;
-
-	/**
-	 * Link to the currently active component with focus.
-	 *
-	 * @readonly
-	 * @type {Component}
-	 */
-	//this.activeComponent = null;
-
 	// sanitize
 	config = config || {};
+
+	// can't accept focus
+	config.focusable = config.focusable || false;
+
+	// hidden
+	config.visible = config.visible || false;
 
 	// parent init
 	Component.call(this, config);
 
 	// correct CSS class names
 	this.$node.classList.add('widget');
-
-	// state flag
-	this.active = this.$node.classList.contains('active');
-
-	// correct DOM parent/child connection if necessary
-	//if ( this.$node.parentNode === null ) {
-	//	document.body.appendChild(this.$node);
-	//}
-
-	// always itself
-	//this.page = this;
 }
 
 
