9c6ebfeb899588a20095cdd64de36f29fb9bd0e6
Stanislav Kalashnik <sk@infomir.eu>
2015-01-26 14:41:45 +0200

    add template app global config

---
+++ config/app.js
@@ -0,0 +1,12 @@
+/**
+ * Global application configuration.
+ * Should store run-time options, paths, flags and so on.
+ *
+ * @author Stanislav Kalashnik <sk@infomir.eu>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+// public export
+module.exports = {};
