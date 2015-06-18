437da1833172d6198dfe29a7bdc5c4b7debc3ee4
Stanislav Kalashnik <sk@infomir.eu>
2015-06-18 12:51:16 +0300

    increase inspect default depth

--- tpl/app/js/stb/develop/debug.js
+++ tpl/app/js/stb/develop/debug.js
@@ -137,7 +137,7 @@ module.exports = window.debug = {
 	 */
 	inspect: function ( data, depth ) {
 		if ( host ) {
-			log('inspect:\n' + util.inspect(data, {depth: depth || 0, colors: true}));
+			log('inspect:\n' + util.inspect(data, {depth: depth || 3, colors: true}));
 		} else {
 			console.log(data);
 		}
