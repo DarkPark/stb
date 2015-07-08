84ad9f1680faec693531ee2f08d3fe19a895b03f
Stanislav Kalashnik <sk@infomir.eu>
2015-07-08 12:10:29 +0300

    add parseUri and parseQuery tools methods

--- tpl/app/js/stb/tools.js
+++ tpl/app/js/stb/tools.js
@@ -8,6 +8,48 @@
 
 
 /**
+ * URL parsing tool
+ * (c) Steven Levithan <stevenlevithan.com>
+ * MIT License
+ *
+ * @param {string} str string to parse
+ *
+ * @return {string} result data
+ */
+function parseUri ( str ) {
+	var o   = parseUri.options,
+		m   = o.parser[o.strictMode ? 'strict' : 'loose'].exec(str),
+		uri = {},
+		i   = 14;
+
+	while ( i-- ) { uri[o.key[i]] = m[i] || ''; }
+
+	uri[o.q.name] = {};
+	uri[o.key[12]].replace(o.q.parser, function ( $0, $1, $2 ) {
+		if ( $1 ) { uri[o.q.name][$1] = $2; }
+	});
+
+	return uri;
+}
+
+parseUri.options = {
+	strictMode: false,
+	key       : ['source', 'protocol', 'authority', 'userInfo', 'user', 'password', 'host', 'port', 'relative', 'path', 'directory', 'file', 'query', 'anchor'],
+	q         : {
+		name  : 'queryKey',
+		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+	},
+	parser    : {
+		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+		loose : /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+	}
+};
+
+
+module.exports.parseUri = parseUri;
+
+
+/**
  * Do string substitution according to the given format.
  * http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
  *
@@ -26,4 +68,31 @@ module.exports.format = function ( format ) {
 	return format.replace(/{(\d+)}/g, function ( match, number ) {
 		return args[number] !== undefined ? args[number] : match;
 	});
+};
+
+
+/**
+ * Parse the given location search string into object.
+ *
+ * @param {string} query string to parse
+ *
+ * @return {Object} result data
+ *
+ * @example
+ * console.log(parseQuery(document.location.search.substring(1)));
+ * console.log(parseQuery('param=value&another_param=another_value'));
+ */
+module.exports.parseQuery = function ( query ) {
+	var data = {};
+
+	// parse and fill the data
+	query.split('&').forEach(function ( part ) {
+		part = part.split('=');
+		// valid number on params
+		if ( part.length === 2 ) {
+			data[part[0]] = decodeURIComponent(part[1]);
+		}
+	});
+
+	return data;
 };
