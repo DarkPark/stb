e7c6db28e12f11e5ff79714d56e5b5a1f68fd43a
DarkPark <darkpark@pisem.net>
2015-07-26 22:42:09 +0300

    rework Panel

--- tpl/app/js/stb/ui/panel.js
+++ tpl/app/js/stb/ui/panel.js
@@ -40,11 +40,17 @@ function Panel ( config ) {
 	// sanitize
 	config = config || {};
 
+	if ( DEBUG ) {
+		if ( typeof config !== 'object' ) { throw new Error(__filename + ': wrong config type'); }
+		// init parameters checks
+		if ( config.className && typeof config.className !== 'string' ) { throw new Error(__filename + ': wrong or empty config.className'); }
+	}
+
 	// can't accept focus
 	config.focusable = config.focusable || false;
 
 	// set default className if classList property empty or undefined
-	config.className = config.className || 'panel';
+	config.className = 'panel ' + (config.className || '');
 
 	// parent constructor call
 	Component.call(this, config);
