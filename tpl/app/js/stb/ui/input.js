d0c8be8c4a22f580462065c7898ab3e95798a79b
Stanislav Kalashnik <sk@infomir.eu>
2015-07-27 15:58:58 +0300

    improve Input dom construction

--- tpl/app/js/stb/ui/input.js
+++ tpl/app/js/stb/ui/input.js
@@ -19,9 +19,6 @@ var Component = require('../component'),
  * @extends Component
  *
  * @param {Object} [config={}] init parameters (all inherited from the parent)
- * @param {Element} [config.$caret] DOM element/fragment to be a input caret element
- * @param {Element} [config.$placeholder] DOM element/fragment to be a element for input placeholder
- * @param {function} [config.navigate] method to move focus according to pressed keys
  * @param {string} [config.value='text'] input text value
  * @param {string} [config.placeholder='password'] placeholder text value
  * @param {string} [config.type=Input.TYPE_TEXT] input type
@@ -39,9 +36,6 @@ var Component = require('../component'),
  *     });
  */
 function Input ( config ) {
-	// current execution context
-	//var self = this;
-
 	// sanitize
 	config = config || {};
 
@@ -49,7 +43,6 @@ function Input ( config ) {
 		if ( typeof config !== 'object' ) { throw new Error(__filename + ': wrong config type'); }
 		// init parameters checks
 		if ( config.className && typeof config.className !== 'string'   ) { throw new Error(__filename + ': wrong or empty config.className'); }
-		//if ( config.navigate  && typeof config.navigate  !== 'function' ) { throw new Error(__filename + ': wrong config.navigate type'); }
 	}
 
 	/**
@@ -58,20 +51,6 @@ function Input ( config ) {
 	 * @type {string}
 	 */
 	this.value = '';
-
-	/**
-	 * Hint element with placeholder text.
-	 *
-	 * @type {Element}
-	 */
-	this.$placeholder = null;
-
-	/**
-	 * Caret element, which shows current cursor position.
-	 *
-	 * @type {Element}
-	 */
-	this.$caret = null;
 
 	/**
 	 * Input type, now available only text and password.
@@ -83,71 +62,32 @@ function Input ( config ) {
 	 */
 	this.type = this.TYPE_TEXT;
 
-	/**
-	 * Direction of the symbols in input.
-	 *
-	 * @type {string}
-	 */
-	this.direction = 'ltr';
-
 	// set default className if classList property empty or undefined
 	config.className = 'input ' + (config.className || '');
 
 	// parent constructor call
 	Component.call(this, config);
 
-	// create $body if not provided
-	if ( this.$node === this.$body ) {
-		// insert text line
-		this.$body = this.$node.appendChild(document.createElement('div'));
+	// insert text line
+	this.$line = this.$body.appendChild(document.createElement('div'));
+	// correct class
+	this.$line.className = 'line';
 
-		// correct class
-		this.$body.className = 'body';
-		// $caret creation if not provided
-		this.$caret = this.$body.appendChild(document.createElement('div'));
-		// correct class
-		this.$caret.className = 'caret';
+	// element to show current cursor position
+	this.$caret = this.$line.appendChild(document.createElement('div'));
+	// correct class
+	this.$caret.className = 'caret';
 
-		// $placeholder creation if not provided
-		this.$placeholder = this.$body.appendChild(document.createElement('div'));
-		// correct class
-		this.$placeholder.className = 'placeholder';
-	} else {
-		if ( DEBUG ) {
-			if ( !config.$caret ) { throw new Error(__filename + ': config.$caret node must be provided'); }
-			if ( !(config.$caret instanceof Element) ) { throw new Error(__filename + ': wrong config.$caret type'); }
-			if ( !config.$placeholder ) { throw new Error(__filename + ': $placeholder node must be provided'); }
-			if ( !(config.$placeholder instanceof Element) ) { throw new Error(__filename + ': wrong config.$placeholder type'); }
-		}
-		// custom nodes
-		this.$caret = config.$caret;
+	// hint element with placeholder text
+	this.$placeholder = this.$line.appendChild(document.createElement('div'));
+	// correct class
+	this.$placeholder.className = 'placeholder';
 
-		this.$placeholder = config.$placeholder;
-	}
 	// setup caret index
 	this.$caret.index = 0;
 
-	if ( DEBUG ) {
-		if ( !this.$body.classList.contains('body') ) { throw new Error(__filename + ': $body node must have "body" token in className'); }
-		if ( !this.$caret.classList.contains('caret') ) { throw new Error(__filename + ': $caret node must have "caret" token in className'); }
-		if ( !this.$placeholder.classList.contains('placeholder') ) { throw new Error(__filename + ': $placeholder node must have "placeholder" token in className'); }
-	}
-
 	// component setup
 	this.init(config);
-
-	// custom navigation method
-	//if ( config.navigate ) {
-	//	// apply
-	//	this.navigate = config.navigate;
-	//}
-
-	// navigation by keyboard
-	//this.addListener('keydown', this.navigate);
-
-	//this.addListener('keypress', function ( event ) {
-	//	self.addChar(String.fromCharCode(event.keyCode), self.$caret.index);
-	//});
 }
 
 
@@ -216,54 +156,6 @@ Input.prototype.defaultEvents = {
 
 
 /**
- * Default method to move focus according to pressed keys.
- *
- * @param {Event} event generated event source of movement
- */
-//Input.prototype.navigateDefault = function ( event ) {
-//	switch ( event.code ) {
-//		case keys['delete']:
-//			this.removeChar(this.$caret.index);
-//			break;
-//
-//		case keys.back:
-//			this.removeChar(this.$caret.index - 1);
-//			break;
-//
-//		case keys.left:
-//			this.setCaretPosition(this.$caret.index - 1);
-//			break;
-//
-//		case keys.right:
-//			this.setCaretPosition(this.$caret.index + 1);
-//			break;
-//
-//		case keys.end:
-//		case keys.down:
-//			this.setCaretPosition(this.value.length);
-//			break;
-//
-//		case keys.home:
-//		case keys.up:
-//			this.setCaretPosition(0);
-//			break;
-//
-//		default:
-//			break;
-//	}
-//};
-
-
-/**
- * Current active method to move focus according to pressed keys.
- * Can be redefined to provide custom navigation.
- *
- * @type {function}
- */
-//Input.prototype.navigate = Input.prototype.navigateDefault;
-
-
-/**
  * Init or re-init of the component inner structures and HTML.
  *
  * @param {Object} config init parameters (subset of constructor config params)
@@ -297,11 +189,7 @@ Input.prototype.init = function ( config ) {
 	}
 
 	// char direction
-	if ( config.direction ) {
-		// apply
-		this.direction = config.direction;
-	}
-	this.$body.dir = this.direction;
+	this.$line.dir = config.direction || 'ltr';
 };
 
 
@@ -328,7 +216,7 @@ Input.prototype.addChar = function ( char, index ) {
 
 	// remove hint
 	if ( this.value.length === 0 ) {
-		this.$body.removeChild(this.$placeholder);
+		this.$line.removeChild(this.$placeholder);
 	}
 
 	// settings class name for span which presents one symbol in virtual input
@@ -349,11 +237,11 @@ Input.prototype.addChar = function ( char, index ) {
 	}
 
 	if ( index >= this.value.length ) { // add char to the end, move caret to the end
-		this.$body.appendChild($char);
-		this.$body.appendChild(this.$caret);
+		this.$line.appendChild($char);
+		this.$line.appendChild(this.$caret);
 	} else { // move caret before index, append span before caret
-		this.$body.insertBefore(this.$caret, this.$body.children[index]);
-		this.$body.insertBefore($char, this.$caret);
+		this.$line.insertBefore(this.$caret, this.$line.children[index]);
+		this.$line.insertBefore($char, this.$caret);
 	}
 
 	// there are some listeners
@@ -385,11 +273,11 @@ Input.prototype.removeChar = function ( index ) {
 
 		if ( this.$caret.index === index && index < this.value.length ) {
 			// remove char after caret
-			this.$body.removeChild(this.$body.children[index + 1]);
+			this.$line.removeChild(this.$line.children[index + 1]);
 		} else if ( this.$caret.index > index ) {
 			// remove char before caret
 			--this.$caret.index;
-			this.$body.removeChild(this.$body.children[index]);
+			this.$line.removeChild(this.$line.children[index]);
 		}
 
 		// cut one char from the value
@@ -404,7 +292,7 @@ Input.prototype.removeChar = function ( index ) {
 
 	// only hint
 	if ( this.value.length === 0 ) {
-		this.$body.appendChild(this.$placeholder);
+		this.$line.appendChild(this.$placeholder);
 	}
 };
 
@@ -419,14 +307,14 @@ Input.prototype.setCaretPosition = function ( index ) {
 	// check boundaries and current position
 	if ( index >= 0 && index <= this.value.length && this.$caret.index !== index ) {
 		// extract caret
-		this.$body.removeChild(this.$caret);
+		this.$line.removeChild(this.$caret);
 
 		// apply
 		if ( index === this.value.length ) {
 			// add to the end
-			this.$body.appendChild(this.$caret);
+			this.$line.appendChild(this.$caret);
 		} else {
-			this.$body.insertBefore(this.$caret, this.$body.children[index]);
+			this.$line.insertBefore(this.$caret, this.$line.children[index]);
 		}
 
 		this.$caret.index = index;
@@ -457,12 +345,12 @@ Input.prototype.setValue = function ( value ) {
 	// non-empty string
 	if ( newLength > 0 ) {
 		// no hint
-		if ( this.$placeholder.parentNode === this.$body ) {
-			this.$body.removeChild(this.$placeholder);
+		if ( this.$placeholder.parentNode === this.$line ) {
+			this.$line.removeChild(this.$placeholder);
 		}
 
 		// no cursor
-		this.$body.removeChild(this.$caret);
+		this.$line.removeChild(this.$caret);
 
 		// value length has changed
 		if ( newLength !== oldLength ) {
@@ -472,20 +360,20 @@ Input.prototype.setValue = function ( value ) {
 			if ( diff > 0 ) {
 				// add missing chars
 				for ( i = 0; i < diff; i++ ) {
-					$char = this.$body.appendChild(document.createElement('div'));
+					$char = this.$line.appendChild(document.createElement('div'));
 					$char.className = 'char';
 				}
 			} else {
 				// remove unnecessary chars
 				for ( i = 0; i > diff; i-- ) {
-					this.$body.removeChild(this.$body.lastChild);
+					this.$line.removeChild(this.$line.lastChild);
 				}
 			}
 		}
 
 		// apply value
 		for ( i = 0; i < newLength; i++ ) {
-			$char = this.$body.children[i];
+			$char = this.$line.children[i];
 
 			if ( this.type === this.TYPE_PASSWORD ) {
 				$char.innerHTML = '*';
@@ -498,13 +386,13 @@ Input.prototype.setValue = function ( value ) {
 
 		this.value = value;
 		this.$caret.index = newLength;
-		this.$body.appendChild(this.$caret);
+		this.$line.appendChild(this.$caret);
 	} else {
 		// empty string
 		this.value = '';
-		this.$body.innerText = '';
-		this.$body.appendChild(this.$caret);
-		this.$body.appendChild(this.$placeholder);
+		this.$line.innerText = '';
+		this.$line.appendChild(this.$caret);
+		this.$line.appendChild(this.$placeholder);
 	}
 
 	// there are some listeners
