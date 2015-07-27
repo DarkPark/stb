0b8db6a02879945070ec062d7f5c232d895e4236
Stanislav Kalashnik <sk@infomir.eu>
2015-07-27 18:55:39 +0300

    improve ModalBox dom construction

--- tpl/app/js/stb/ui/modal.box.js
+++ tpl/app/js/stb/ui/modal.box.js
@@ -25,22 +25,21 @@ function ModalBox ( config ) {
 		if ( typeof config !== 'object' ) { throw new Error(__filename + ': wrong config type'); }
 		// init parameters checks
 		if ( config.className && typeof config.className !== 'string' ) { throw new Error(__filename + ': wrong or empty config.className'); }
+		if ( config.$body ) { throw new Error(__filename + ': config.$body should not be provided in ModalBox manually'); }
 	}
 
 	// set default className if classList property empty or undefined
 	config.className = 'modalBox ' + (config.className || '');
 
+	// create centered div
+	config.$body = document.createElement('div');
+	config.$body.className = 'body';
+
 	// parent constructor call
 	Component.call(this, config);
 
-	// create $body if not provided
-	if ( this.$node === this.$body ) {
-		// create centered div
-		this.$body = document.createElement('div');
-		this.$body.className = 'body';
-		// add table-cell wrapper
-		this.$node.appendChild(document.createElement('div').appendChild(this.$body).parentNode);
-	}
+	// add table-cell wrapper
+	this.$node.appendChild(document.createElement('div').appendChild(this.$body).parentNode);
 }
 
 
