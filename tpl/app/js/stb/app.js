323f8c319b3b5c58df2d54b49cc1f4326f000cb4
igor <deadbyelpy@gmail.com>
2016-02-24 18:23:17 +0200

    remove localStorage polyfill from releases

--- tpl/app/js/stb/app.js
+++ tpl/app/js/stb/app.js
@@ -16,8 +16,6 @@ var Model    = require('./model'),
 
 require('./shims');
 
-window.localStorage = window.localStorage || window.stbStorage || (window.parent !== window ? window.parent.localStorage || window.parent.stbStorage : null);
-
 // inside frame/iframe
 /*if ( window.parent && window.parent.gSTB ) {
  // link to the outer global objects
