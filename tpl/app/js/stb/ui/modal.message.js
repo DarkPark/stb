023c924718d9d5ebce9f4ed36589ddbb0e7c77d0
Dima Fedotov <1990bas@gmail.com>
2016-03-02 09:12:30 +0200

    replace modal message with modal box ex code

--- tpl/app/js/stb/ui/modal.message.js
+++ tpl/app/js/stb/ui/modal.message.js
@@ -6,16 +6,30 @@
 
 'use strict';
 
-var ModalBox = require('./modal.box.js');
+var Component = require('../component');
 
 
 /**
- * Base modal window implementation.
+ * Modal window implementation.
  *
  * @constructor
- * @extends ModalBox
+ * @extends Component
  *
  * @param {Object} [config={}] init parameters (all inherited from the parent)
+ * @param {Object} [config.title] message title
+ * @param {Object} [config.className] message classname
+ * @param {Object} [config.icon] icon at header
+ * @param {Object} [config.children] content (inherited from the parent)
+ *
+ *
+ * page.modalMessage = new ModalMessage({
+ *        title: 'My Title',
+ *        icon: 'star',
+ *        children: [new Button({value: 'Create'})]
+ *    });
+ * page.add(page.modalBox);
+ * page.modalBox.show();
+ *
  */
 function ModalMessage ( config ) {
     // sanitize
@@ -24,31 +38,50 @@ function ModalMessage ( config ) {
     if ( DEBUG ) {
         if ( typeof config !== 'object' ) { throw new Error(__filename + ': wrong config type'); }
         // init parameters checks
+        if ( config.icon && typeof config.icon !== 'string' ) { throw new Error(__filename + ': wrong or empty config.icon'); }
+        if ( config.title && typeof config.title !== 'string' ) { throw new Error(__filename + ': wrong or empty config.icon'); }
         if ( config.className && typeof config.className !== 'string' ) { throw new Error(__filename + ': wrong or empty config.className'); }
+        if ( config.$body ) { throw new Error(__filename + ': config.$body should not be provided in ModalBox manually'); }
     }
 
     // set default className if classList property empty or undefined
     config.className = 'modalMessage ' + (config.className || '');
 
+    // create centered div
+    config.$body = document.createElement('div');
+    config.$body.className = 'body';
+
     // parent constructor call
-    ModalBox.call(this, config);
+    Component.call(this, config);
 
-    this.$header  = this.$body.appendChild(document.createElement('div'));
-    this.$content = this.$body.appendChild(document.createElement('div'));
-    this.$footer  = this.$body.appendChild(document.createElement('div'));
+    // add table-cell wrappers
+    this.$node.appendChild(document.createElement('div'));
+    this.$node.firstChild.appendChild(document.createElement('div'));
 
-    this.$header.className  = 'header';
-    this.$content.className = 'content';
-    this.$footer.className  = 'footer';
+    // add header div
+    this.$header = document.createElement('div');
+    this.$header.className = 'header';
 
-    this.$header.innerText  = 'header';
-    this.$content.innerText = 'content';
-    this.$footer.innerText  = 'footer';
+    // insert caption placeholder
+    this.$text = this.$header.appendChild(document.createElement('div'));
+    this.$text.classList.add('text');
+    this.$text.innerText = config.title || '';
+
+    // optional icon
+    if ( config.icon ) {
+        // insert icon
+        this.$icon = this.$header.appendChild(document.createElement('div'));
+        this.$icon.className = 'icon ' + config.icon;
+    }
+
+    // add to dom
+    this.$node.firstChild.firstChild.appendChild(this.$header);
+    this.$node.firstChild.firstChild.appendChild(this.$body);
 }
 
 
 // inheritance
-ModalMessage.prototype = Object.create(ModalBox.prototype);
+ModalMessage.prototype = Object.create(Component.prototype);
 ModalMessage.prototype.constructor = ModalMessage;
 
 
